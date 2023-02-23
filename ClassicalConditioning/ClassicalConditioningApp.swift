e9d23d8261fb37ed6e49022cd628af88b48fa028
Nathan Morelli <2005Nathan2005@gmail.com>
2023-02-23 08:13:25 -0500

    Initial Commit

---
+++ ClassicalConditioning/ClassicalConditioningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicalConditioningApp.swift
+//  ClassicalConditioning
+//
+//  Created by Nathan Morelli (student LM) on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ClassicalConditioningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassicalConditioning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassicalConditioning
+//
+//  Created by Nathan Morelli (student LM) on 2/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
