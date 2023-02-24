ae429f899ac1fe7c277a70f9e7e12153c20d69a1
Nathan Morelli <2005Nathan2005@gmail.com>
2023-02-24 10:36:05 -0500

    voices and coin flip

--- ClassicalConditioning/ContentView.swift
+++ ClassicalConditioning/ContentView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 import AVFoundation
-
+import CoreMotion
 struct ContentView: View {
+    @State private var isAnimating = false
+    @State private var coolor = false
+
     var body: some View {
+        //voice 
+        /*
         Button {
             let utterance = AVSpeechUtterance(string: "Hello world")
             utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
@@ -21,6 +26,26 @@ struct ContentView: View {
             Text("Push me")
             
         }
+        */
+        //coin flip
+       
+        Button {
+            isAnimating = true
+            coolor = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                isAnimating = false
+            }
+            } label :  {
+            Image(systemName: "circle.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(coolor ? .yellow : .red)
+                .rotation3DEffect(
+                    .degrees(isAnimating ? 360 : 0),
+                    axis: (x: 1.0, y: 0.0, z: 0.0))
+                .animation(.easeInOut(duration: 0.5))
+        }
+        
     }
 }
 
