1d830db36854cfffe93f8d54cd2b236097e462c6
Nathan Morelli <2005Nathan2005@gmail.com>
2023-02-28 07:50:23 -0500

    Added models folder and main run track file

---
+++ ClassicalConditioning/Views/TrackView.swift
@@ -0,0 +1,20 @@
+//
+//  TrackView.swift
+//  ClassicalConditioning
+//
+//  Created by Nathan Morelli (student LM) on 2/28/23.
+//
+
+import SwiftUI
+
+struct TrackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrackView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackView()
+    }
+}
