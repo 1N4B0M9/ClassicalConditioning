5460b4d254c0b250680802395fb9371657264c5a
EthanLiebs <s026293@students.lmsd.org>
2023-03-13 08:06:02 -0400

    added basic outline for voice prompt system

--- ClassicalConditioning/Track/Tracker.swift
+++ ClassicalConditioning/Track/Tracker.swift
@@ -18,6 +18,8 @@ class Tracker: ObservableObject {
     @Published var distance: Int? //total distance traveled in meters
     @Published var intervals: Int = 0 //testing value
     @Published var intervalsFailed: Int = 0 //testing value
+    private var ticks = 0 //ticks and updates every 30 updates
+    private var progress: TrackerProgress = TrackerProgress(steps: 0, cadence: 0, distance: 0)
     
     init() {
         self.pedometer.startUpdates(from: self.date) { value, error in
@@ -29,6 +31,7 @@ class Tracker: ObservableObject {
             } else {
                 self.intervalsFailed += 1
             }
+            self.prompt()
             /*
             print("____________________________________")
             print("steps \(self.steps ?? -1)")
@@ -41,7 +44,43 @@ class Tracker: ObservableObject {
         }
     }
     
+    /*
+     Called when tracker is no longer used
+     */
     func stop() {
         self.pedometer.stopUpdates()
+    }
+    
+    /*
+     Called on every update tick
+     */
+    func prompt() {
+        self.ticks += 1
+        guard self.ticks % 30 == 0 else { //every 30 ticks prompt user
+            return
+        }
+        
+        if let steps = self.steps, let cadence = self.cadence, let distance = self.distance, self.progress.meetsConditions(steps: steps, cadence: cadence, distance: distance) {
+            //read out "good job" in a straightforward or backhand way based on mode
+            self.progress = TrackerProgress(steps: steps, cadence: cadence, distance: distance)
+        } else {
+            //read out encourement that user "can do better" based on mode
+        }
+    }
+}
+
+private struct TrackerProgress {
+    let steps: Int
+    let cadence: Int
+    let distance: Int
+    
+    init(steps: Int, cadence: Int, distance: Int) {
+        self.steps = steps
+        self.cadence = cadence
+        self.distance = distance
+    }
+    
+    func meetsConditions(steps: Int, cadence: Int, distance: Int) -> Bool {
+        return true //determine if current values meet conditions
     }
 }
