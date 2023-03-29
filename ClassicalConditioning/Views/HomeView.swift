d45893d1f50b326b576f6ed139974a0ec63bbfee
Nathan Morelli <2005Nathan2005@gmail.com>
2023-03-29 08:09:49 -0400

    ui change 4 gradient

--- ClassicalConditioning/Views/HomeView.swift
+++ ClassicalConditioning/Views/HomeView.swift
@@ -14,11 +14,11 @@ struct HomeView: View {
     var body: some View {
         ZStack{
             if madOrHappy.madHappy == false {
-                RadialGradient(gradient: Gradient(colors: [.red, .white]), center: .center, startRadius: 2, endRadius: 650)
+                RadialGradient(gradient: Gradient(colors: [.white, .red]), center: .center, startRadius: 2, endRadius: 650)
                     .ignoresSafeArea()
             }
             else {
-                RadialGradient(gradient: Gradient(colors: [.blue, .white]), center: .center, startRadius: 2, endRadius: 650)
+                RadialGradient(gradient: Gradient(colors: [.white, .blue]), center: .center, startRadius: 2, endRadius: 650)
                     .ignoresSafeArea()
 
             }
