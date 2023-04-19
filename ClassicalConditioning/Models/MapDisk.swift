2586fa10b836740437917704f450890bef9ab8e2
EthanLiebs <s026293@students.lmsd.org>
2023-04-19 10:10:18 -0400

    removed some debugs

--- ClassicalConditioning/Models/MapDisk.swift
+++ ClassicalConditioning/Models/MapDisk.swift
@@ -15,8 +15,6 @@ struct MapViewDisk : UIViewRepresentable {
     @State var polylineArr : [MKPolyline] = []
     init(coords : [CLLocationCoordinate2D]) {
         self.coords = coords
-        print("cords size = \(self.coords.count)") //debug
-        print("cords = \(self.coords)") //debug
     
         reg.center = CLLocationCoordinate2D(latitude: coords[1].latitude, longitude: coords[1].longitude)
         
