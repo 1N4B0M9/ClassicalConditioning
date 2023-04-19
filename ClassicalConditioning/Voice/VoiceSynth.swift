79e2c73a19b059921777fed42a62fbc3af82231f
EthanLiebs <s026293@students.lmsd.org>
2023-04-19 10:40:22 -0400

    changed session nonsense to work even if ringer off

--- ClassicalConditioning/Voice/VoiceSynth.swift
+++ ClassicalConditioning/Voice/VoiceSynth.swift
@@ -21,7 +21,15 @@ class Sound {
         case v1, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14, h15, h16
     }
     
-    private init() {}
+    private init() {
+        let session = AVAudioSession.sharedInstance()
+        
+        do {
+            try session.setCategory(.playback)
+        } catch let error {
+            print("Error setting category. \(error.localizedDescription)")
+        }
+    }
     
     func play(_ happyOrMad: Bool) {
         print("play audio") //debug
